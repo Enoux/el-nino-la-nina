248a4d9ad52eb85c5cd8763dfd1a304c68b32add
Benjeouelle Lazaro <balazaro1@up.edu.ph>
2026-03-23 21:26:06 +0800

    feat: save state of collectible items

--- ElNinoLaNina/Assets/Scripts/CollectibleItem.cs
+++ ElNinoLaNina/Assets/Scripts/CollectibleItem.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -5,11 +7,29 @@ public class CollectibleItem : MonoBehaviour
 {
     public ItemData item;
     public InventoryManager playerInventory;
+    private List<String> hotspotStates;
+    private int hotspotIdx;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        
+        hotspotStates = PlayerSaveFile.currentSaveFile.hotspotStateKeys;
+        string name = this.gameObject.name;
+
+        // GameObject already in hotspotStates
+        if (hotspotStates.Contains(name))
+        {   
+            hotspotIdx = hotspotStates.IndexOf(name);
+
+            // Item already collected; should not exist anymore
+            if (PlayerSaveFile.currentSaveFile.hotspotStateVals[hotspotIdx] == 1) Destroy(this.gameObject);
+        }
+
+        else
+        {
+            PlayerSaveFile.currentSaveFile.hotspotStateKeys.Add(name);
+            PlayerSaveFile.currentSaveFile.hotspotStateVals.Add(0);
+            hotspotIdx = hotspotStates.Count - 1;
+        }
     }
 
     // Update is called once per frame
@@ -48,6 +68,7 @@ public class CollectibleItem : MonoBehaviour
         {   
             playerInventory.AddItem(item);
             PlayerSaveFile.currentSaveFile.playerItems = playerInventory.GetItems();
+            PlayerSaveFile.currentSaveFile.hotspotStateVals[hotspotIdx] = 1;
             Destroy(this.gameObject);
             return true;
         }
