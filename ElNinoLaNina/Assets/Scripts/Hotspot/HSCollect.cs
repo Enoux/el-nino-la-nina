bba23666f4a8d91e5a7001f3d15e8676ae94bb98
hugvan <vaughnaquino@gmail.com>
2026-04-02 16:15:20 +0800

    feat: add CollectHS

---
+++ ElNinoLaNina/Assets/Scripts/Hotspot/HSCollect.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor.PackageManager;
+using NUnit.Framework;
+
+public class HSCollect : HSInteract
+{
+    [SerializeField]
+    private GameObject collectibleItem;
+    [SerializeField]
+    private ItemData collectibleItemData;
+
+    void Start() {
+        UpdateFromState();
+        UpdateCollider(); // NOTE: This somehow fixes colliders
+    }
+
+    public override void OnInteract(ItemData item) {
+        InventoryManager.Instance.AddItem(collectibleItemData);
+        state = 1;
+        UpdateFromState();
+    }
+
+    private void UpdateFromState() {
+        if (state == 1) {
+            EnableCollider(false);
+            collectibleItem.SetActive(false);
+        } else if (state == 0) {
+            EnableCollider(true);
+        } else {
+            throw new System.Exception(name + " in Invalid State.");
+        }
+    }
+
+    public override void OnHoverEnterTrigger() {
+        SetCollectibleItemLayer(8);
+        base.OnHoverEnterTrigger();
+    }
+
+    public override void OnHoverExitTrigger() {
+        SetCollectibleItemLayer(0);
+        base.OnHoverExitTrigger();
+    }
+
+    void SetCollectibleItemLayer(int layer) {
+        collectibleItem.layer = layer;
+        foreach (Transform child in collectibleItem.transform) {
+            child.gameObject.layer = layer;
+        }
+    }
+}
